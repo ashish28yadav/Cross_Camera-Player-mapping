726631ad195b0c293f61fd79c4b4ca534afeea19
Ashish Ranjan <149693115+ashish28yadav@users.noreply.github.com>
2025-07-19 11:17:23 +0530

    Delete model.py

--- model.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-# Correct: Load YOLOv8 model
-model = YOLO(r"C:\Users\ranja\DDA Python\best (1).pt")
-
-# Run prediction on an image or frame
-results = model("test_image.jpg")  # Or use a video frame
-results[0].show()
